39d9e8f2b5bc6ea5c41a39dc4dc83b2a57f4945a
ewggrodno <ewggrodno@gmail.com>
2018-06-06 00:21:53 +0300

    Added class HibernateUtil(SessionFactory)

---
+++ src/main/java/bl/HibernateUtil.java
@@ -0,0 +1,31 @@
+package bl;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e){
+            System.out.println("Initial SessionFactory creation failed. " + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+    public static void shutdown(){
+        getSessionFactory().close();
+    }
+
+    public static void main(String[] args) {
+        getSessionFactory();
+        shutdown();
+    }
+}
